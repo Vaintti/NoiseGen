9ed6be5cdde4992f00cc23392cbc8cce2a80570c
Vaintti <Vaintti@users.noreply.github.com>
2020-07-31 00:27:31 +0300

    Optimize by saving possible colors in a list

--- NoiseGen/Program.cs
+++ NoiseGen/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using FreeImageAPI;
@@ -9,6 +10,12 @@ namespace NoiseGen
     {
         static void Main(string[] args)
         {
+            var colors = new List<RGBQUAD>();
+            foreach (int i in Enumerable.Range(0, 255))
+            {
+                colors.Add(new RGBQUAD(Color.FromArgb(i, i, i)));
+            }
+
             var resolution = 1024;
             try
             {
@@ -17,8 +24,8 @@ namespace NoiseGen
             {
                 Console.WriteLine("Couldn't parse resolution number from input.");
             }
-            var bitmap = FreeImage.Allocate(resolution, resolution, 24);
 
+            var bitmap = FreeImage.Allocate(resolution, resolution, 24);
             var rand = new Random();
             
             foreach (uint y in Enumerable.Range(0, resolution))
@@ -26,7 +33,7 @@ namespace NoiseGen
                 foreach (uint x in Enumerable.Range(0, resolution))
                 {
                     var randomValue = rand.Next(0, 255);
-                    var color = new RGBQUAD(Color.FromArgb(randomValue, randomValue, randomValue));
+                    var color = colors[randomValue];
                     FreeImage.SetPixelColor(bitmap, x, y, ref color);
                 }
             }
